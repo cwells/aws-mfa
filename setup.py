94f1355b5b483743432721f0127a7ef629a62c9f
Cliff Wells <cwells@focusvision.com>
2018-06-19 11:22:32 -0700

    make requirements.txt canonical source for requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup
 
 setup(
-  name='aws-mfa',
-  version='1.0',
-  py_modules=['aws_mfa'],
-  install_requires=[
-    'boto3>=1.5.20',
-    'click>=6.7',
-    'PyYAML>=3.12'
-  ],
-  entry_points='''
+  name       = 'aws-mfa',
+  version    = '1.0',
+  py_modules = ['aws_mfa'],
+
+  install_requires = open('requirements.txt').readlines(),
+
+  entry_points = '''
     [console_scripts]
     aws-mfa=aws_mfa:cli
-  ''',
+  '''
 )
