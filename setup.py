cf105fd6656e9619b88faf04fbe97f865aa62995
Cliff Wells <cwells@focusvision.com>
2018-06-19 14:55:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name       = 'aws-mfa',
-  version    = '1.2',
+  version    = '1.3',
   py_modules = ['aws_mfa'],
 
   install_requires = open('requirements.txt').readlines(),
